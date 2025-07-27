6c84fee86bf9288c5b7fa92d0cde39f9011a9a8f
mohammed <mohammedaliyu33@gmail.com>
2025-07-27 11:11:38 +0100

    modified entry point to allow for argument parsing in CLI

--- country_picker/__main__.py
+++ country_picker/__main__.py
@@ -1,5 +1,24 @@
-    
+"""
+Entry point for country_picker
+
+Allows package to be run with `python -m country_picker`.
+Handles CLI for pre-selecting country.
+"""
+import argparse
 from country_picker.main import run
 
 if __name__ == "__main__":
-        run()
+    # parses arguments
+    parser = argparse.ArgumentParser(
+        description="A simple app to select a country from a list."
+    )
+    parser.add_argument(
+        "--select",
+        type=str,
+        metavar="COUNTRY",
+        help="The country to pre-select on startup."
+    )
+    args = parser.parse_args()
+
+    # passes pre-selected country to run function
+    run(selected_country=args.select)
